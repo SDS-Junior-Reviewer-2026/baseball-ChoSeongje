39ebe3bef857ad6f2a52fa7ad877db978edc86f2
choseongje <hangoy0606@gmail.com>
2026-04-03 13:34:13 +0900

    [feature] 59p

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,7 +1,9 @@
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class GameTest {
 
@@ -35,5 +37,14 @@ public class GameTest {
         assertIllegalArgument("121");
     }
 
+    @Test
+    void returnSolvedResultIfMatchedNumber() {
+        game.question = "123";
+        GuessResult result = game.guess("123");
 
+        assertThat(result).isNotNull();
+        assertThat(result.solved).isEqualTo(true);
+        assertThat(result.strikes).isEqualTo(3);
+        assertThat(result.balls).isEqualTo(0);
+    }
 }
