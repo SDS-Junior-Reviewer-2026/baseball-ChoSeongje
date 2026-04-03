906e64e0d8155dc66ed07049461e7ef8f9e4474e
choseongje <hangoy0606@gmail.com>
2026-04-03 13:45:11 +0900

    [feature] 70p

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -61,5 +61,11 @@ public class GameTest {
 
         assertMatchedNumber(game.guess("456"), false, 0, 0);
     }
-    
+
+    @Test
+    void returnSolvedResultIfSomeMatchedNumber() {
+        generateQuestion("123");
+
+        assertMatchedNumber(game.guess("120"), false, 2, 0);
+    }
 }
