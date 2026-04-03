653cc1af1facf13e4af3dcf2e5b15b0b82de8a1a
choseongje <hangoy0606@gmail.com>
2026-04-03 13:15:08 +0900

    [feature] 34p

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,3 +1,19 @@
-public class GameTest {
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GameTest {
+    @Test
+    void createGame() {
+        Game game = new Game();
+        assertNotNull(game);
+    }
+
+    @Test
+    void throwExceptionInputIsNull() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Game game = new Game();
+            game.guess(null);
+        });
+    }
 }
