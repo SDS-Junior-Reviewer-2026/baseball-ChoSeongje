1a68f1903265e5999321db168b9addd69919048d
choseongje <hangoy0606@gmail.com>
2026-04-03 13:25:38 +0900

    [feature] 49p

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -31,6 +31,8 @@ public class GameTest {
         assertIllegalArgument(null);
         assertIllegalArgument("12");
         assertIllegalArgument("1234");
+        assertIllegalArgument("12s");
+        assertIllegalArgument("121");
     }
 
 
